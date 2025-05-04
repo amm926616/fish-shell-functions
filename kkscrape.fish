2b968e7009276164b5c462a3f5bdd64ec7c8c904
Aung Myint Myat <amm926616@gmail.com>
2025-05-04 21:25:46 +0630

    added update nap

---
+++ kkscrape.fish
@@ -0,0 +1,4 @@
+function kkscrape --wraps=/home/aiden178/Programming/amm-git-repos/kitty-kats-scrapper/kkscrape.py --description 'alias kkscrape /home/aiden178/Programming/amm-git-repos/kitty-kats-scrapper/kkscrape.py'
+  /home/aiden178/Programming/amm-git-repos/kitty-kats-scrapper/kkscrape.py $argv
+        
+end
---
+++ update-nap.fish
@@ -0,0 +1,4 @@
+function update-nap --wraps=/home/aiden178/Programming/amm-git-repos/amm-code-snippets/update.sh --description 'alias update-nap /home/aiden178/Programming/amm-git-repos/amm-code-snippets/update.sh'
+  /home/aiden178/Programming/amm-git-repos/amm-code-snippets/update.sh $argv
+        
+end
